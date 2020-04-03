94b3edb296f9c99d9799020e384e1d08b8ac231b
Nicolashide <57710446+Nicolashide@users.noreply.github.com>
2020-04-04 02:02:58 +0300

    Update echo_bot.py

--- echo_bot.py
+++ echo_bot.py
@@ -1,3 +1,16 @@
 import telebot
 
 bot = telebot.TeleBot("828754275:AAGNadQbQW_PH96dfKU9RHItkmep9jmoIEQ")
+import telebot
+
+bot = telebot.TeleBot("TOKEN")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling()
