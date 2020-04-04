7ce4289997f7a6ba89f6bf17f1bfb26bd4c6b59f
Nicolashide <57710446+Nicolashide@users.noreply.github.com>
2020-04-04 07:04:10 +0300

    Update echo_bot.py

--- echo_bot.py
+++ echo_bot.py
@@ -1,9 +1,7 @@
 import telebot
 
 bot = telebot.TeleBot("828754275:AAGNadQbQW_PH96dfKU9RHItkmep9jmoIEQ")
-import telebot
 
-bot = telebot.TeleBot("TOKEN")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
