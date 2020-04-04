b73d36c5ce4d447708416614b54e9fd0dc3186a2
Nicolashide <57710446+Nicolashide@users.noreply.github.com>
2020-04-04 07:44:29 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+import telebot
+TOKEN = "828754275:AAEjbgAFxd0go8JHdw5cnMryfzgQbuwZnuM"
+bot = telebot.TeleBot(TOKEN)
+ 
+# Приветственная надпись
+@bot.message_handler(commands=['start'])
+def start(message):
+    sent = bot.send_message(message.chat.id, 'Как тебя зовут?')
+    bot.register_next_step_handler(sent, hello)
+ 
+def hello(message):
+    bot.send_message(message.chat.id, 'Привет, {name}. Рад тебя видеть.'.format(name=message.text))
+ 
+bot.polling()
